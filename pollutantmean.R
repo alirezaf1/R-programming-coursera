340646daa0ec4ee1310d2cc38b5a3489764e5db7
alirezaf1 <alireza.farhidzadeh@gmail.com>
2017-03-08 12:04:46 -0500

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,10 +1,9 @@
 pollutantmean <- function(directory, pollutant, id){
   
-  ##dir  <-  "C:/Users/alireza.farhidzadeh/Dropbox/Courses/R programming/Week 2/Programming assignment/specdata"
+  ##dir  <-  "C:/Users/alireza/Dropbox/Courses/R programming/Week 2/Programming assignment/specdata"
 
   temp <-  list.files(path=directory, pattern = ".csv")
-  ## myfiles <-  lapply(temp[id], read.delim)
- 
+
   data <- NULL
   for (i in id){
     datatemp <- read.csv(paste(directory, "/" ,temp[i], sep = ""), header = T, skip = 0)
